3a88e14c1561558b102ab072ba5f18dca71d3a11
unknown <nikh0881@LJ21YGV1.RACKSPACE.CORP>
2013-05-09 15:17:42 -0500

    removed GUI for the application, as it required more effort to populate the fields in the html,will be added in next iteration, improving directory structure

--- todolist/settings.py
+++ todolist/settings.py
@@ -11,24 +11,22 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        # The following settings are not used with sqlite3:
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '',                      # Set to empty string for default.
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'C:/Users/nikh0881/IdeaProjects/todolist_app/todolist/datastore/todolist.sqlite3',                      # Or path to database file if using sqlite3.
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
-
-# Hosts/domain names that are valid for this site; required if DEBUG is False
-# See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = []
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
-# In a Windows environment this must be set to your system time zone.
+# On Unix systems, a value of None will cause Django to use the same
+# timezone as the operating system.
+# If running in a Windows environment this must be set to the same as your
+# system time zone.
 TIME_ZONE = 'America/Chicago'
 
 # Language code for this installation. All choices can be found here:
@@ -49,22 +47,22 @@ USE_L10N = True
 USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
-# Example: "/var/www/example.com/media/"
+# Example: "/home/media/media.lawrence.com/media/"
 MEDIA_ROOT = ''
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
-# Examples: "http://example.com/media/", "http://media.example.com/"
+# Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
 MEDIA_URL = ''
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
-# Example: "/var/www/example.com/static/"
+# Example: "/home/media/media.lawrence.com/static/"
 STATIC_ROOT = ''
 
 # URL prefix for static files.
-# Example: "http://example.com/static/", "http://static.example.com/"
+# Example: "http://media.lawrence.com/static/"
 STATIC_URL = '/static/'
 
 # Additional locations of static files
@@ -83,7 +81,7 @@ STATICFILES_FINDERS = (
 )
 
 # Make this unique, and don't share it with anybody.
-SECRET_KEY = 'nz473d@he#*txc!i!n0d4ew56_dkxa6btb6u%h@^(&tf^wf2c8'
+SECRET_KEY = ')!&amp;b8#r89$n6c$q8&amp;zu#bx&amp;&amp;w!j4o9l2u(=o*((203tucc10of'
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
@@ -107,7 +105,22 @@ ROOT_URLCONF = 'todolist.urls'
 # Python dotted path to the WSGI application used by Django's runserver.
 WSGI_APPLICATION = 'todolist.wsgi.application'
 
-TEMPLATE_DIRS = ('C:/Users/nikh0881/IdeaProjects/Django_todolist/todolist/todolist/templates',)
+TEMPLATE_DIRS = (
+    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+)
+
+TEMPLATE_CONTEXT_PROCESSORS = (
+"django.contrib.auth.context_processors.auth",
+"django.core.context_processors.debug",
+"django.core.context_processors.i18n",
+"django.core.context_processors.media",
+"django.core.context_processors.static",
+"django.core.context_processors.tz",
+"django.contrib.messages.context_processors.messages",
+'django.core.context_processors.csrf',
+)
 
 INSTALLED_APPS = (
     'django.contrib.auth',
@@ -116,11 +129,11 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'todolist',
     # Uncomment the next line to enable the admin:
-    #'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
-    #'django.contrib.admindocs',
+    'django.contrib.admindocs',
+    'todolist.todolist.handler',
 )
 
 # A sample logging configuration. The only tangible logging
@@ -151,3 +164,7 @@ LOGGING = {
         },
     }
 }
+
+LOGIN_REDIRECT_URL = "/admin/"
+
+PISTON_DISPLAY_ERRORS = True
---
+++ todolist/urls.py
@@ -0,0 +1,40 @@
+from django.conf.urls import patterns, include, url
+
+from piston.resource import Resource
+from piston.authentication import HttpBasicAuthentication
+
+from todolist.handler.api import TodoItemHandler
+
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+
+
+admin.autodiscover()
+
+
+urlpatterns = patterns('',
+    # Examples:
+    url(r'^$', 'todolist.handler.views.index', name='index'),
+    url(r'^register$', 'todolist.handler.views.register', name='register'),
+    # url(r'^todolist/', include('todolist.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    url(r'^admin/', include(admin.site.urls)),
+
+    #(r'^accounts/login/$', 'django.contrib.auth.views.login', {'template_name': 'admin/login.html'}),
+    (r'^accounts/login/$', 'django.contrib.auth.views.login', {'template_name': 'login.html'}),
+    (r'^accounts/logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}),
+)
+
+
+auth = HttpBasicAuthentication(realm="API Realm")
+todoapiresource = Resource(TodoItemHandler, authentication=auth)
+urlpatterns += patterns('',
+   url(r'^api/items$', todoapiresource),
+   url(r'^api/item/(?P<todoitem_id>[^/]+)?$', todoapiresource),
+)
+
