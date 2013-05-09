bd3776a6ea8c3a6ca637576601a6f27501318261
unknown <nikh0881@LJ21YGV1.RACKSPACE.CORP>
2013-05-09 15:21:32 -0500

    modifying the model object, accomodating choices to the fields

--- todolist/handler/models.py
+++ todolist/handler/models.py
@@ -2,12 +2,38 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+PRIORITY_CHOICES = (
+    (u'0', u'LOW'),
+    (u'1', u'NORMAL'),
+    (u'2', u'MEDIUM'),
+    (u'3', u'HIGH'),
+    )
+
+
+CATEGORY_CHOICES = (
+    (u'0', u'GENERAL'),
+    (u'1', u'WORK'),
+    (u'2', u'PERSONAL'),
+    )
+
+
+STATUS_CHOICES=(
+    (u'0', u'NEW'),
+    (u'1', u'DUE'),
+    (u'2', u'PERSONAL'),
+    )
+
+
+
+
 
 class TodoItem(models.Model):
     user = models.ForeignKey(User)
     todo = models.TextField()
-    priority = models.IntegerField(blank=True, null=True)
-    
+    priority = models.IntegerField(blank=True, null=True,choices=PRIORITY_CHOICES)
+    created=models.DateTimeField()
+    status=models.IntegerField(blank=True, null=True,choices=STATUS_CHOICES)
+    category= models.IntegerField(blank=True, null=True,choices=CATEGORY_CHOICES)
     def __unicode__(self):
         if self.priority:
             return self.todo + " (priority: %s)" % self.priority
