f7e061e9f72a73983e5746cc47e1fa63d3536ced
unknown <nikh0881@LJ21YGV1.RACKSPACE.CORP>
2013-05-09 15:48:33 -0500

    adding URL mapping to the resource objects

--- todolist/urls.py
+++ todolist/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import patterns, include, url
 from piston.resource import Resource
 from piston.authentication import HttpBasicAuthentication
 
-from todolist.handler.api import TodoItemHandler
+from handler.todoapi import TodosHandler
 
 
 # Uncomment the next two lines to enable the admin:
@@ -17,24 +17,20 @@ urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'todolist.handler.views.index', name='index'),
     url(r'^register$', 'todolist.handler.views.register', name='register'),
-    # url(r'^todolist/', include('todolist.foo.urls')),
-
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
-
-    #(r'^accounts/login/$', 'django.contrib.auth.views.login', {'template_name': 'admin/login.html'}),
     (r'^accounts/login/$', 'django.contrib.auth.views.login', {'template_name': 'login.html'}),
     (r'^accounts/logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}),
 )
 
-
-auth = HttpBasicAuthentication(realm="API Realm")
-todoapiresource = Resource(TodoItemHandler, authentication=auth)
+#these are the calls which need authentication of the user
+auth = HttpBasicAuthentication(realm="API")
+todoresource = Resource(TodosHandler, authentication=auth)
 urlpatterns += patterns('',
-   url(r'^api/items$', todoapiresource),
-   url(r'^api/item/(?P<todoitem_id>[^/]+)?$', todoapiresource),
+   url(r'^todo/list$', todoresource),
+   url(r'^todo/task/(?P<todoitem_id>[^/]+)?$', todoresource),
 )
 
